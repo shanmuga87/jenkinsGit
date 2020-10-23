4b6127fc5ef1b8ade7f1d492fc45a49a29ccbc4f
shanmuga87 <psn.mca@gmail.com>
2020-10-23 06:21:32 -0700

    committed

--- Sample.java
+++ Sample.java
@@ -2,8 +2,8 @@ public class Helloworld
 {
   public static void main(String args[])
 {
-  //System.out.println("Welcome to java.....");
-    //System.out.println("Welcome to java.....");
-	//System.out.println("Welcome to java.....");
+  System.out.println("Welcome to java.....");
+  System.out.println("Welcome to java.....");
+	System.out.println("Welcome to java.....");
 }
 }
